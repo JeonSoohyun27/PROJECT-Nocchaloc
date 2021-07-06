950a401b33825ed2df6cc9f18177cedaa605bb93
anjaekk <anjaekk@gmail.com>
2021-07-06 20:41:28 +0900

    Add: models

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db  import models
+
+class User(models.Model):
+    name         = models.CharField(max_length=50)
+    birthday     = models.CharField(max_length=50)
+    phone_number = models.IntegerField(unique=True)
+    account      = models.CharField(max_length=50,unique=True)
+    password     = models.CharField(max_length=200)
+    point        = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'users'
