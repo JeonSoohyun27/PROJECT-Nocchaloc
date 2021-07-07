652e3ab6fd971cc8e0eb14e446fd57e259daadcf
JeonSoohyun27 <jeonsoohyun27@gmail.com>
2021-07-07 20:39:54 +0900

    nocchaloc 로그인/회원가입 뷰2차 완성

--- users/views.py
+++ users/views.py
@@ -1,23 +1,30 @@
 import re,json,bcrypt,jwt
 
-from django.core.exceptions import MultipleObjectsReturned
 from django.views           import View
 from django.http            import JsonResponse
 from django.shortcuts       import render
 
 from users.models           import User
-from users.validator        import validate_account, validate_password
 from my_settings            import SECRET_KEY
+from datetime               import datetime, timedelta
 
 # Create your views here.
 
+REGEX = {
+    'account'  : '[a-zA-Z]\w{4,12}',
+    'password' : '(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,16}'
+}
+
 class SignupView(View):
     def post(self, request):
-        data = json.loads(request.body)
         try:
-            if not validate_password(data['password']) or validate_account(data['account']):
+            data     = json.loads(request.body)
+            account  = data['account']
+            password = data['password']
+
+            if not re.match(REGEX['account'],account) and re.match(REGEX['password'],password):
                 return JsonResponse({'message':'INVALID_ERROR'},status=404)
-            
+
             if User.objects.filter(account=data['account']).exists() or User.objects.filter(phone_number=data['phone_number']).exists():
                 return JsonResponse({'message':'DUPLICATE'},status=409)
 
@@ -31,7 +38,7 @@ class SignupView(View):
                 password     = encoded_password.decode('utf-8'),
                 point        = 50000,
             )
-            return JsonResponse({'message':'SUCCESS'},status=200)
+            return JsonResponse({'message':'SUCCESS'},status=201)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'},status=400)
@@ -39,19 +46,21 @@ class SignupView(View):
 class SigninView(View):
     def post(self, request):
         try:
-            data     = json.loads(request.body)
+            data = json.loads(request.body)
+            
+            if not User.objects.filter(account=data['account']).exists():
+                return JsonResponse({'message':'INVALID_USER'},status=401)
+
             account  = data['account']
             password = data['password']
-            user     = User.objects.get(account=data['account'])
+            user_id  = User.objects.get(account=account).id
 
-            if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
-                encoded_jwt = jwt.encode({'account':user.account},SECRET_KEY,algorithm='HS256')
+            if bcrypt.checkpw(data['password'].encode('utf-8'),User.password.encode('utf-8')):
+                encoded_jwt = jwt.encode({'user_id':user_id,'exp':datetime.utcnow()+timedelta(days=1)},SECRET_KEY,algorithm='HS256')
                 return JsonResponse({'message':'SUCCESS','TOKEN':encoded_jwt},status=200)
-            
-            return JsonResponse({'message':'INPUT_ERROR'},status=406)
 
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'},status=400)
 
-        except MultipleObjectsReturned:
-            return JsonResponse({'message':'INVALID_ERROR'},status=404)
+        except ValueError:
+            return JsonResponse({'message':'INPUT_ERROR'},status=400)
