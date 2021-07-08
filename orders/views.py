fa942474c26c80a89852e1f5804ead43f3c29ae1
anjaekk <anjaekk@gmail.com>
2021-07-08 17:32:00 +0900

    Modify: cart_view

--- orders/views.py
+++ orders/views.py
@@ -5,13 +5,12 @@ from django.http   import JsonResponse
 from orders.models import Cart
 
 class CartView(View):
+    @authorization
     def get(self, request):
         try:
-            data = json.loads(request.body)  # 토큰 적용시 삭제
-            if Cart.objects.filter(user_id=data['user']):  # 토큰유저 수정
-                user  = data['user']
-                carts = Cart.objects.filter(user_id=user)
-                user  = {'user_id' : user} # 토큰유저 수정
+            if Cart.objects.filter(user_id=request.user).exists():
+                carts = Cart.objects.filter(user_id=request.user)
+                user  = {'user_id' : request.user}
                 
                 cart_list = [{
                     'product' : cart.product.name,
@@ -19,11 +18,23 @@ class CartView(View):
                     'option'  : cart.option.option,  # name으로 변경
                     'price'   : cart.product.price,
                 } for cart in carts]
-
                 return JsonResponse({'user':user, 'cart_list':cart_list}, status=200)
+#==============================================================
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
         except TypeError:
             return JsonResponse({'message':'TYPE_ERROR'}, status=400)
         except  ValueError:
             return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
+
+class CartDeleteView(View):
+    @authorization
+    def post(sele, request):
+        try:
+            data = json.loads(request.body)
+            user = request.user_id
+
+            if Cart.objects.filter(user_id=user, cart_id=data['cart']).exists:
+                Cart.objects.get(user_id=user, cart_id=data['cart']).delete()
+                return JsonResponse({'message':'SUCCESS'}, staus=200)
+            return 
