52aa652f3b75cc01a205bc32b01e90dd935709c1
박성준 <seong_jun@jun.local>
2021-07-09 17:14:39 +0900

    git products_view

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from orders.views import CartAddUpdateView, CartDeleteView, CartView
+from orders.views import CartView
 
 urlpatterns = [
     path('/cart', CartView.as_view()),
