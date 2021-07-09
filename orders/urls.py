0ffebf468dcaaea52ca7907033706f8ad89d111d
anjaekk <anjaekk@gmail.com>
2021-07-09 11:21:53 +0900

    Add: Cart delete view

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from orders.views import CartAddUpdateView, CartDeleteView, CartView
 
 urlpatterns = [
     path('/cart', CartView.as_view()),
+    path('/cart/delete', CartDeleteView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -14,15 +14,36 @@ class CartView(View):
                 carts = Cart.objects.filter(user_id=request.user.id)
 
                 cart_list = [{
-                    'product'      : cart.product.name,
-                    'quantity'     : cart.quantity,
-                    'option'       : cart.option.name,
-                    'unit_price'   : cart.product.price,
-                    'price'        : cart.product.price * cart.quantity
+                    'product'    : cart.product.name,
+                    'quantity'   : cart.quantity,
+                    'option'     : cart.option.name,
+                    'unit_price' : cart.product.price,
+                    'price'      : cart.product.price * cart.quantity
                 } for cart in carts]
 
                 return JsonResponse({'user':request.user.id, 'cart_list':cart_list}, status=200)
             return JsonResponse({'message':'VALUE_ERROR'}, status=404)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        except TypeError:
+            return JsonResponse({'message':'TYPE_ERROR'}, status=400)
+        except  ValueError:
+            return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
+
+class CartDeleteView(View):
+    @authorization
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            
+            for cart in data['cart_id']:
+                if not Cart.objects.filter(user=request.user, id=cart).exists:
+                    return JsonResponse({'message':'VALUE_ERROR'}, status=404)
+
+            for cart in data['cart_id']:
+                Cart.objects.get(user=request.user, id=cart).delete()
+
+            return JsonResponse({'message':'SUCCESS'}, status=200)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
         except TypeError:
