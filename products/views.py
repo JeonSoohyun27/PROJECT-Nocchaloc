48a2c4d015f4fff7ee408ae9bb40c158d9b09b07
박성준 <seong_jun@jun.local>
2021-07-13 14:11:20 +0900

    git products_view10

--- products/views.py
+++ products/views.py
@@ -1,15 +1,17 @@
 from django.db.models import Q
 from django.http import JsonResponse
 from django.views import View
-from products.models import Product, Category, ProductType, Option
+from products.models import Product, Category, Option
 
 class ProductView(View):
+
     def get(self, request):
         sort_dic = {
             '1':'-is_new',
-            '2':'price',
-            '3':'-price',
+            '2':'-price',
+            '3':'price',
             'm':'-view_count'}
+        MAIN_AMOUNT  = 10
         product_type = request.GET.getlist('product_type', None)
         category     = request.GET.get('category', None)
         sort         = request.GET.get('sort', '1')
@@ -20,10 +22,10 @@ class ProductView(View):
             q &= Q(category_id=category)
         if product_type:
             q &= Q(product_type_id__in=product_type)
-        if page == 'm':
-            products = Product.objects.filter(q).order_by(sort_dic[page])[:10]
-        else:
-            products = Product.objects.filter(q).order_by(sort_dic[sort])
+
+        products = Product.objects.filter(q).order_by(sort_dic[sort])
+        if page =='m':
+            products = Product.objects.filter(q).order_by(sort_dic[page])[0:MAIN_AMOUNT]
 
         products_info = [{
             "name"            : product.name,
